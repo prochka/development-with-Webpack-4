d32009f5c23b5ad0e8bbde3faa045a2a485553ab
Prochka <mr.dvorezky@gmail.com>
2019-12-29 13:55:48 +0300

    подключение jquery ui с помощью jquery-ui-bundle

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,18 @@
 import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import "../sass/style.scss";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 require.context("../images/", true, /\.(png|svg|jpg|gif)$/);
 import $ from 'jquery';
+import 'jquery-ui-bundle';
+
 import result from './custom.js'
+
+
 
 result($);
 
